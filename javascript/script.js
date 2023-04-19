94c30c156ee9d9d731ceb8abdea9d34b0b9a5566
Apekshavaity07 <98224195+Apekshavaity07@users.noreply.github.com>
2023-04-20 00:29:40 +0530

    Update files

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+ var swiper = new Swiper(".slide-content", {
+        slidesPerView: 4,
+        centeredSlides: 'true',
+        spaceBetween: 30,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+
+    });
